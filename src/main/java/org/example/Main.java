b9145a54293540a85b6fb32bc4fc79f65359d94d
Bekesh Dastan <bekesdastan@gmail.com>
2025-10-31 21:56:22 +0500

    Kahn is done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,23 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        Graph g = new Graph(8);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 3);
+        g.addEdge(2, 3);
+        g.addEdge(3, 4);
+        g.addEdge(4, 5);
+        g.addEdge(5, 6);
+        g.addEdge(5, 7);
 
+        kahn k = new kahn(g);
+
+        for (int i=0;i<g.vertices;i++){
+                System.out.println(k.getTopOrder(i));
+                if (k.getTopOrder(i) == "Graph has a cycle. Topological sort not possible."){
+                    return;
+                }
+        }
     }
 }
--- src/main/java/org/example/kahn.java
+++ src/main/java/org/example/kahn.java
@@ -1,8 +1,52 @@
 package org.example;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class kahn {
+    int[] inDegree;
+    int[] topOrder;
+    int index = 0;
+    boolean hasCycle=false;
 
-    public kahn(Graph G){
+    public kahn(Graph G) {
+        inDegree = new int[G.vertices];
+        topOrder = new int[G.vertices];
+        for (int u = 0; u < G.vertices; u++) {
+            for (int v : G.adjList.get(u)) {
+                inDegree[v]++;
+            }
+        }
+        Queue<Integer> queue = new LinkedList<>();
 
+        for (int i = 0; i < G.vertices; i++) {
+            if (inDegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int u = queue.poll();
+            topOrder[index++] = u;
+
+            for (int v : G.adjList.get(u)) {
+                inDegree[v]--;
+                if (inDegree[v] == 0) {
+                    queue.add(v);
+                }
+
+            }
+        }
+
+        if (index < G.vertices) {
+            hasCycle = true;
+        }
+    }
+
+    public String getTopOrder(int i){
+        if (hasCycle) {
+            return "Graph has a cycle. Topological sort not possible.";
+        }
+        return Integer.toString(topOrder[i]);
     }
 }
